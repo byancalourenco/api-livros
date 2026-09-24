37c9aab61dcf36287ac5a6104f24ed26e80ff492
byanca lourenço <159471845+byancalourenco@users.noreply.github.com>
2026-09-24 08:44:02 -0300

    Add files via upload

--- database/biblioteca_db.sql
+++ database/biblioteca_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17-Set-2026 às 14:18
+-- Tempo de geração: 24-Set-2026 às 13:40
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `biblioteca_db`
 --
+CREATE DATABASE IF NOT EXISTS `biblioteca_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `biblioteca_db`;
 
 -- --------------------------------------------------------
 
